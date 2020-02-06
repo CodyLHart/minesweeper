52f8df8f360e736338d21115370ea42383e0df6a
Cody Hart <codyhart@Codys-MacBook-Pro-5.local>
2020-02-05 22:05:58 -0700

    Write Psuedocode

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//Define required constants
+    // Different types of squares: empty, mine, number of mines touching
+
+//Define required variables used to track the state of the game
+    // Number of mines - number
+    // Board (locations of mines) - array (probably?)
+    // Game over - boolean
+    // Winner - boolean
+    // Timer
+    // Message - string
+    // High scores (technically low times) - array of numbers
+
+//Store elements on the page that will be accessed in code
+    // Store all of the cells on the board (10x10, eventually will be able to change)
+
+// Upon loading the app should:
+    // Initialize the state variables
+        // Let the player select how many mines
+        // Let the player select board dimensions
+        // Randomize the mine locations and place appropriate number locations
+        // let gameOver = false;
+        // let isWinner = false;
+    // Render those values to the page
+    // Wait for the user to click a square
+
+// Handle if player clicks a square
+    // If player clicks their first square
+        // Start the timer
+    // If cell contains a mine
+        // gameOver
+    // Otherwise - If cell is touching at least one mine
+        // Reveal the number of mines that cell is touching
+    // Otherwise - If cell is touching no mines
+        // Reveal all connected cells that are touching no mines
+        // Reveal a border of numbers surrounding the blank cells
+    // Make all revealed squares unclickable
+
+// Handle if player right clicks a square
+    // Place a flag in that square
+    // Make that square unclickable unless you right click it again to remove the flag
+
+// If gameOver 
+    // Stop timer
+    // Reveal replay button
+    // If winner
+        // Winner message
+        // If time is in top scores, save it to the top scores
+    // If loser
+        // Reveal remaning mines
+        // Loser message
+        
+// Handle a player clicking the replay button
+    // Just reinitiate
+

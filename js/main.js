8fefeece172a0451761e94ae950b024472f95345
Cody Hart <codyhart@Codys-MacBook-Pro-5.local>
2020-02-07 15:15:39 -0700

    Update Psuedocode and Add Wireframe

--- js/main.js
+++ js/main.js
@@ -1,31 +1,45 @@
 //Define required constants
-    // Different types of squares: empty, mine, number of mines touching
+    // Types of cells
+        // Mine cell
+        // Empty cell
+        // Number cell
+        // Flag cell
+        // Hidden cell
 
 //Define required variables used to track the state of the game
-    // Number of mines - number
-    // Board (locations of mines) - array (probably?)
-    // Game over - boolean
-    // Winner - boolean
-    // Number of remaining flags 
-    // Timer
-    // Message - string
-    // High scores (technically low times) - array of numbers
+    // Difficulty Level
+        // numOfMines - number
+        // board (locations of mines) - array
+            // - array of arrays of numbers to represent each cell's value
+    // gameOver - boolean
+    // isWinner - boolean
+    // numOfFlags - number
+    // timer
+    // message - string
+    // fastTimes - array of numbers
 
-//Store elements on the page that will be accessed in code
-    // Store all of the cells on the board (10x10, eventually will be able to change)
+// Cached DOM elements
+    // All of the cells on the board 
+    // Replay button
+    // Message
+
 
 // Upon loading the app should:
-    // Initialize the state variables
-        // Let the player select how many mines
-        // Let the player select board dimensions
-        // Randomize the mine locations and place appropriate number locations
-        // let gameOver = false;
-        // let isWinner = false;
+    // Receive player input for difficulty level
+    // Create the board accordinly
+        // Initialize the board as an array of arrays of nums
+            // Maybe a 1 dimensional array... I haven't decided yet
+            // These nums represent the value at each cell
+            // Initialize these to null
+        // Randomize the mine locations (mines have a value or -1)
+        // gameOver = false;
+        // isWinner = false;
+        // message  = 'Good Luck!' or something
     // Render those values to the page
-    // Wait for the user to click a square
+    // Wait for the user to click a cell
 
-// Handle if player clicks a square
-    // If player clicks their first square
+// Handle if player clicks a cell
+    // If this is the first cell they have clicked
         // Start the timer
     // If cell contains a mine
         // gameOver
@@ -34,16 +48,18 @@
     // Otherwise - If cell is touching no mines
         // Reveal all connected cells that are touching no mines
         // Reveal a border of numbers surrounding the blank cells
-    // Make all revealed squares unclickable
+        // This part sounds hard. We shall see.
+    // Make sure all revealed cells are unclickable
 
 // Handle if player right clicks 
-    // If cell is unmarked
-        // Place a flag in that square
-        // Make that square un-left-clickable
+    // If cell is unflagged
+        // Give cell a flag
+        // Make that cell un-left-clickable
+            // If cell has class of flagged, remove event listener? Probably...
         // Decrease remaining flags available by 1
-    // If cell is marked
-        // Remove flag from square
-        // Make that square left-clickable again
+    // If cell is flagged
+        // Remove flag from cell
+        // Make that cell left-clickable again 
         // Increase remaining flags available by 1
 
 // If gameOver 
@@ -56,6 +72,9 @@
         // Reveal remaning mines
         // Loser message
         
-// Handle a player clicking the replay button
-    // Just reinitiate
-
+    // I think I want the overall opacity of the board to fade down to maybe 
+    // 30-50% while the gameOver div shows up full opacity over top of it.
+    // Along with the gameOver message, will be the replay button
+    // When user presses the replay button, the div will ask the user 
+    // for game difficulty. Upon selection, the board will render and 
+    // fade back in to full opacity.
